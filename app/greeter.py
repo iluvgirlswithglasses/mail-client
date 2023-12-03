725ff6787a6bb09e8b1bb1bbff64ac571ebc71bf
iluvgirlswithglasses <luunamdat204@gmail.com>
2023-12-03 22:35:21 +0700

    upd config + additional greeter text + what inbox threading should look like

--- app/greeter.py
+++ app/greeter.py
@@ -13,6 +13,7 @@ I firmly believe in the supremacy of the Euphonium
 FYI I use Debian
 """
 
+from colorama import Fore, Style
 from app.printer import Printer
 from app.sender import CSender
 from app.inbox import CInbox
@@ -48,16 +49,35 @@ class CGreeter(Printer):
             CInbox(self.user)
         self.iterate()
 
+    def titlelog(self, msg: str):
+        self.skipln()
+        print(Fore.RED + "    " + msg + Style.RESET_ALL)
+        self.skipln()
+
     def greet(self):
         self.scrclr()
         self.gdelog(self.sepbar)
+
+        self.titlelog("1. Required Environment")
+
+        self.gdelog("This program is expected to work ONLY on POSIX systems, which are")
+        self.gdelog("Linux, MacOS, OpenBDS, et cetera, and NOT Windows. This is because the")
+        self.gdelog("techniques used in handling the 7-th requirement only work on POSIX's")
+        self.gdelog("terminal. In detail, I wrote a threading & console manipulation module")
+        self.gdelog("that works on shells like bash/zsh/fish, but does not works on PS.")
         self.skipln()
+        self.gdelog("If you get an error while logging in, it means that you are not using")
+        self.gdelog("a POSIX system, and therefore, this program will not work at all.")
+
+        self.titlelog("2. About This Project")
+
         self.gdelog("This project was made to show our understanding of SMTP/POP3 protocol,")
         self.gdelog("not to show our application making skill. Thus, the interface of this")
         self.gdelog("program is keep as simple as possible, and thus it's not idiot-proof.")
         self.skipln()
         self.gdelog("Be precise at your inputs. Exceptions are not handled.")
         self.skipln()
+
         self.gdelog(self.sepbar)
         self.skipln()
 
--- app/inbox.py
+++ app/inbox.py
@@ -13,9 +13,90 @@ I firmly believe in the supremacy of the Euphonium
 FYI I use Debian
 """
 
+import os, time, threading
+from config import Config
 from app.printer import Printer
 
 class CInbox(Printer):
     def __init__(self, user):
         super().__init__()
+        self.user = user
+        self.allow_upd = False  # allow self.drawing_thread() to works
+        self.upd_itval = Config.load()['refresh_interval']
+        self.initdir()
+        self.iterate()
+
+    def iterate(self):
+        choice = self.askdir()
+        if choice == 0:
+            return
+        self.viewdir(choice)
+        self.iterate()
+
+    """
+    @ comms
+    """
+    def askdir(self):
+        userdir = f'saved_mail/{self.user}'
+        ls = [os.path.join(userdir, x) for x in os.listdir(userdir)]
+
+        self.greet()
+        self.syslog("Available mail directories:")
+        for i, d in enumerate(ls):
+            if os.path.isdir(d):
+                self.syslog(f'    {i+1} - {os.path.basename(d)}')
+        self.syslog("You may type '0' to cancel this operation")
+
+        self.askinp("Index of your desired directory: ")
+        choice = int(input())
+        if choice == 0:
+            return 0
+        return ls[choice - 1]
+
+    def viewdir(self, targ: str):
+        self.greet()
+
+        self.allow_upd = True
+        t = threading.Thread(target=self.drawing_thread, args=(targ, ))
+        t.start()
+
+        self.syslog("Type in the index of the mail you want to read")
+        self.askinp("Your choice: ")
+        choice = int(input())
+        self.allow_upd = False  # stop updating the mail list
+
+    """
+    @ utils
+    """
+    def greet(self):
+        self.scrclr()
+        self.gdelog(self.sepbar)
+        self.skipln()
+        self.gdelog("This is the mail receiving scene.")
+        self.gdelog("Be precise at your inputs. Exceptions are not handled.")
+        self.skipln()
+        self.gdelog(self.sepbar)
+        self.skipln()
+
+    def initdir(self):
+        ls = ['Inbox', 'Important', 'Project', 'Work', 'Spam']
+        for i in ls:
+            targ = f'saved_mail/{self.user}/{i}'
+            if not os.path.exists(targ):
+                os.makedirs(targ)
+
+    """
+    @ threading
+    """
+    def drawing_thread(self, targ: str):
+        while True:
+            ls = os.listdir(targ)
+            if self.allow_upd:
+                self.draw(ls)
+            else:
+                return
+            time.sleep(self.upd_itval)
+
+    def draw(self, ls):
+        print(ls)
 
