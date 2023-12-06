1e170ac73306678a76b4c71485fa622dcca803a5
iluvgirlswithglasses <luunamdat204@gmail.com>
2023-12-06 17:35:14 +0700

    conf utils

--- pop3/classifier.py
+++ pop3/classifier.py
@@ -1,18 +1,9 @@
 
 from pop3.separator import Separator
+from config import Config
 
 class Classifier:
-    IMPORTANT = ['urgent', 'asap', 'important', 'action required', 'critical',
-                 'priority', 'attention', 'dealine', 'approval required', 'emergency',
-                 'important information', 'right now']
-    SPAM = ['virus', 'hack', 'crack', 'security alert', 'suspicious activity',
-            'unauthorized access', 'account compromise', 'fraud warning', 'phishing attempt',
-            'please confirm your identity', 'click here to reset your password', 'verify your account',
-            'unusual login activity', 'your account will be suspended', 'bank account verification',
-            'important security upadate', 'win a prize', 'win a lottery']
-    WORK = ['meeting', 'report', 'project update', 'task', 'collaboration', 'discussion', 'schedule',
-            'feedback', 'assignment']
-    PROJECT = ['dang@gmail.com']  # can be modify
+    conf = Config.load()['keywords']
 
     @staticmethod
     def contains(response, lst):
@@ -25,13 +16,13 @@ class Classifier:
     def classify_email(response):
         content, attac = Separator.separate_content(response)
 
-        if Classifier.contains(content, Classifier.PROJECT):
+        if Classifier.contains(content, Classifier.conf['project']):
             return "Project"
-        if Classifier.contains(content, Classifier.IMPORTANT):
+        if Classifier.contains(content, Classifier.conf['important']):
             return "Important"
-        if Classifier.contains(content, Classifier.SPAM):
+        if Classifier.contains(content, Classifier.conf['spam']):
             return "Spam"
-        if Classifier.contains(content, Classifier.WORK):
+        if Classifier.contains(content, Classifier.conf['work']):
             return "Work"
         return "Inbox"
 
